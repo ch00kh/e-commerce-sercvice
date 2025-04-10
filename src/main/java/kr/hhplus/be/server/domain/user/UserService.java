608a1b521ad79f989d9278fa6de2be2cfc7871e1
ch00kh <pleasantckh@naver.com>
2025-04-10 10:31:26 +0900

    feat: 사용자 조회 기능 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/user/UserService.java
@@ -0,0 +1,23 @@
+package kr.hhplus.be.server.domain.user;
+
+import kr.hhplus.be.server.domain.user.dto.UserCommand;
+import kr.hhplus.be.server.domain.user.entity.User;
+import kr.hhplus.be.server.domain.user.repository.UserRepository;
+import kr.hhplus.be.server.global.exception.ErrorCode;
+import kr.hhplus.be.server.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findByUserId(UserCommand.Find command) {
+        return userRepository.findById(command.id())
+                .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND));
+    }
+}
