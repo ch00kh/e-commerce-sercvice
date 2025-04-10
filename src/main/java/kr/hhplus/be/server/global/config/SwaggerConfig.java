616a92733d0df46e9e6c9159b3242f833cbb9ce0
ch00kh <pleasantckh@naver.com>
2025-04-10 10:29:17 +0900

    move: infra 패키지로 이동

--- src/main/java/kr/hhplus/be/server/global/config/SwaggerConfig.java
+++ src/main/java/kr/hhplus/be/server/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.common.config.swagger;
+package kr.hhplus.be.server.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
