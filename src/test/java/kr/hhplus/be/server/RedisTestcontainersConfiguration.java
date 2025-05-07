dc0aee16873d11a0c038eca6aa622a9046d62f87
ch00kh <pleasantckh@naver.com>
2025-05-08 02:43:44 +0900

    feat: 레디스 테스트 컨테이너 설정

---
+++ src/test/java/kr/hhplus/be/server/RedisTestcontainersConfiguration.java
@@ -0,0 +1,26 @@
+package kr.hhplus.be.server;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+public class RedisTestcontainersConfiguration {
+
+    public static final GenericContainer<?> REDIS_CONTAINER;
+
+    static {
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+                .withExposedPorts(6379);
+        REDIS_CONTAINER.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (REDIS_CONTAINER.isRunning()) {
+            REDIS_CONTAINER.stop();
+        }
+    }
+
+}
